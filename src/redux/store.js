42efb40aa5f6bd8a2178197ebabcdee3fb8581d9
zhangyunzhi <zhangyunzhi@coolgua.com>
2022-01-18 21:44:41 +0800

    'modified:异步action'

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import {createStore} from 'redux'
+import {createStore,applyMiddleware} from 'redux'
 import countReducer from './count_reducer'
 // redux-thunk用于支持action异步任务
 import thunk from 'redux-thunk'
 
-export default createStore(countReducer)
+export default createStore(countReducer,applyMiddleware(thunk))
